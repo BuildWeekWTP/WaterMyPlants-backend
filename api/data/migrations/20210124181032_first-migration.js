2469272ed1c97eb91a77b1328f4401d825888b18
hans0 <kiessler.hans@gmail.com>
2021-05-24 16:39:23 -0400

    heroku push

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -2,9 +2,10 @@ exports.up = async (knex) => {
   await knex.schema
     .createTable('users', (users) => {
       users.increments('user_id')
-      users.string('user_username', 200).notNullable()
-      users.string('user_password', 200).notNullable()
       users.string('user_email', 320).notNullable()
+      // users.string('user_username', 200).notNullable()
+      users.string('user_password', 200).notNullable()
+      users.string('user_phone_number', 20).notNullable()
       users.timestamps(false, true)
     })
 }
